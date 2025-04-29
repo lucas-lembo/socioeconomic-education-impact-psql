21461c241ebbf3ffca5c93c8fb398cbe0841ad09
rafael-dosso <rafael.dosso@gmail.com>
2025-04-29 09:20:37 -0300

    Adding constants.py

---
+++ constants.py
@@ -0,0 +1,123 @@
+niveis_escolaridade = {
+    1: "Sem instrução",
+    2: "Ensino Fundamental Incompleto",
+    3: "Ensino Fundamental Completo",
+    4: "Ensino Médio Incompleto",
+    5: "Ensino Médio Completo",
+    6: "Ensino Superior Incompleto",
+    7: "Ensino Superior Completo",
+    8: "Pós-graduação"
+}
+
+UF_TO_STATE_NAME = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+}
+
+niveis_socio_economicos = {
+    1: {
+        'qtdGeladeiras': 1,
+        'qtdTelevisores': 1,
+        'qtdBanheiros': 1,
+        'qtdCelulares': 1,
+        'qtdQuartos': 2,
+        'qtdCarros': 0,
+        'qtdComputadores': 0,
+        'idEscolaridadePais': 3
+    },
+    2: {
+        'qtdGeladeiras': 1,
+        'qtdTelevisores': 1,
+        'qtdBanheiros': 1,
+        'qtdCelulares': 2,
+        'qtdQuartos': 2,
+        'qtdCarros': 0,
+        'qtdComputadores': 0,
+        'idEscolaridadePais': 5
+    },
+    3: {
+        'qtdGeladeiras': 1,
+        'qtdTelevisores': 1,
+        'qtdBanheiros': 1,
+        'qtdCelulares': 3,
+        'qtdQuartos': 3,
+        'qtdCarros': 0,
+        'qtdComputadores': 0,
+        'idEscolaridadePais': 5
+    },
+    4: {
+        'qtdGeladeiras': 1,
+        'qtdTelevisores': 2,
+        'qtdBanheiros': 1,
+        'qtdCelulares': 3,
+        'qtdQuartos': 3,
+        'qtdCarros': 1,
+        'qtdComputadores': 1,
+        'idEscolaridadePais': 5
+    },
+    5: {
+        'qtdGeladeiras': 1,
+        'qtdTelevisores': 1,
+        'qtdBanheiros': 2,
+        'qtdCelulares': 3,
+        'qtdQuartos': 3,
+        'qtdCarros': 1,
+        'qtdComputadores': 1,
+        'idEscolaridadePais': 5
+    },
+    6: {
+        'qtdGeladeiras': 1,
+        'qtdTelevisores': 2,
+        'qtdBanheiros': 2,
+        'qtdCelulares': 3,
+        'qtdQuartos': 3,
+        'qtdCarros': 1,
+        'qtdComputadores': 2,
+        'idEscolaridadePais': 7
+    },
+    7: {
+        'qtdGeladeiras': 2,
+        'qtdTelevisores': 3,
+        'qtdBanheiros': 3,
+        'qtdCelulares': 4,
+        'qtdQuartos': 4,
+        'qtdCarros': 2,
+        'qtdComputadores': 3,
+        'idEscolaridadePais': 7
+    },
+    8: {
+        'qtdGeladeiras': 3,
+        'qtdTelevisores': 4,
+        'qtdBanheiros': 4,
+        'qtdCelulares': 4,
+        'qtdQuartos': 4,
+        'qtdCarros': 3,
+        'qtdComputadores': 3,
+        'idEscolaridadePais': 7
+    },
+}
