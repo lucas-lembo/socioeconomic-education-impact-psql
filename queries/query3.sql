8f4f26febe2972a372da7580eab3515bfb6736b9
lucas-lembo <lucaslembodelara@hotmail.com>
2025-05-19 16:56:42 -0300

    finishing last query

---
+++ queries/query3.sql
@@ -0,0 +1,14 @@
+SELECT
+    m.uf,
+    est.nome AS nome_estado,
+    saeb."anoMedicao",
+    AVG(saeb."notaPadronizada") AS media_nota_saeb,
+    AVG(taxas.total) AS media_taxa_aprovacao
+FROM public."NotaSAEB" saeb
+JOIN public."Escola" e ON saeb."idEscola" = e."idEscola"
+JOIN public."Municipio" m ON e."idMunicipio" = m."idMunicipio"
+JOIN public."Estado" est ON m.uf = est.uf
+JOIN public."TaxasAprovacao" taxas ON taxas."idEscola" = e."idEscola" AND taxas."anoMedicao" = saeb."anoMedicao"
+WHERE saeb."anoMedicao" IN (2017, 2019, 2021, 2023)
+GROUP BY m.uf, est.nome, saeb."anoMedicao"
+ORDER BY m.uf, saeb."anoMedicao";
